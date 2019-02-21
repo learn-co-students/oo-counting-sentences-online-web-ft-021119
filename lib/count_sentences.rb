38248f3fde65de9719aa89cc8c374c12d45b9dab
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-21 07:03:38 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else 
+      false 
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true 
+    else 
+      false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else 
+      false 
+    end 
   end
 
   def count_sentences
-
+    counter = 0 
+    self.split.each do |word|
+      if word.end_with?(".", "!", "?")
+        counter += 1 
+      end
+    end
+    counter
   end
 end
